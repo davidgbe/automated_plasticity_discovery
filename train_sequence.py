f8c5f37f1bb788af8d70712f801f497dcd838b70
David Bell <davidgrebell@gmail.com>
2023-08-10 12:18:58 -0700

    400 activations, retain large batch size

--- train_sequence.py
+++ train_sequence.py
@@ -44,7 +44,7 @@ np.random.seed(args.seed)
 SEED = args.seed
 POOL_SIZE = args.pool_size
 BATCH_SIZE = args.batch
-N_INNER_LOOP_RANGE = (199, 200) # Number of times to simulate network and plasticity rules per loss function evaluation
+N_INNER_LOOP_RANGE = (399, 400) # Number of times to simulate network and plasticity rules per loss function evaluation
 STD_EXPL_RULES = args.std_expl[0]
 STD_EXPL_CONSTS= args.std_expl[1]
 STD_EXPL_INH= args.std_expl[2]
@@ -127,7 +127,7 @@ if not os.path.exists('sims_out'):
 # Make subdirectory for this particular experiment
 time_stamp = str(datetime.now()).replace(' ', '_')
 joined_l1 = '_'.join([str(p) for p in L1_PENALTIES])
-out_dir = f'sims_out/decoder_ee_3_extended_bipop_c_{BATCH_SIZE}_STD_EXPL_{STD_EXPL_RULES}_{STD_EXPL_CONSTS}_{STD_EXPL_INH}_FIXED_{FIXED_DATA}_L1_PENALTY_{joined_l1}_ACT_PEN_{args.asp}_CHANGEP_{CHANGE_PROB_PER_ITER}_FRACI_{FRAC_INPUTS_FIXED}_SEED_{SEED}_{time_stamp}'
+out_dir = f'sims_out/decoder_ee_3_extended_bipop_d_{BATCH_SIZE}_STD_EXPL_{STD_EXPL_RULES}_{STD_EXPL_CONSTS}_{STD_EXPL_INH}_FIXED_{FIXED_DATA}_L1_PENALTY_{joined_l1}_ACT_PEN_{args.asp}_CHANGEP_{CHANGE_PROB_PER_ITER}_FRACI_{FRAC_INPUTS_FIXED}_SEED_{SEED}_{time_stamp}'
 os.mkdir(out_dir)
 
 # Make subdirectory for outputting CMAES info
