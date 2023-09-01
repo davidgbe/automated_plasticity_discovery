95c01e3ce6372bd89034cbf9892de99d6ca5833d
DAVID BELL <davidgbe@klone-login01.hyak.local>
2023-09-01 16:14:58 -0700

    new csv_writer update

--- csv_writer.py
+++ csv_writer.py
@@ -1,9 +1,9 @@
 import csv
 import numpy as np
 
-def write_csv(out_file_path, data):
+def write_csv(out_file_path, data, delimiter=' '):
 	with open(out_file_path, 'a', newline='\n') as csv_file:
-		writer = csv.writer(csv_file, delimiter=' ')
+		writer = csv.writer(csv_file, delimiter=delimiter)
 
 		if type(data) is np.ndarray:
 			if len(data.shape) > 1:
@@ -13,9 +13,9 @@ def write_csv(out_file_path, data):
 				writer.writerow(data)
 
 		elif type(data) is list:
-			if len(data) > 0 and type(data[0]) is list:
+			if len(data) > 0 and (type(data[0]) is list or type(data[0]) is np.ndarray):
 				for data_row in data:
-					writer.writerow(data_row)
+					writer.writerow(list(data_row))
 			else:
 				writer.writerow(data)
 		else:
