74ab6aa6af327de235fc7c421ce85b8f6b887d27
DAVID BELL <davidgbe@klone-login01.hyak.local>
2023-09-08 14:16:15 -0700

    changes to ila's rules to pump up n number

--- train_sequence.py
+++ train_sequence.py
@@ -53,7 +53,7 @@ FRAC_INPUTS_FIXED = args.frac_inputs_fixed
 INPUT_RATE_PER_CELL = 80
 N_RULES = 4
 N_TIMECONSTS = 4
-REPEATS = 3
+REPEATS = 100
 
 T = 0.11 # Total duration of one network simulation
 dt = 1e-4 # Timestep
