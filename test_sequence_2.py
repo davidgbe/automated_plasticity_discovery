da100860d8501905c3eab5c181a4f250d26a5aa2
DAVID BELL <davidgbe@klone-login03.hyak.local>
2024-04-06 14:13:42 -0700

    graphs of dynamics split out and total connectivity

--- test_sequence_2.py
+++ test_sequence_2.py
@@ -619,7 +619,7 @@ if __name__ == '__main__':
 	# file_names = [ROOT_FILE_NAME]
 	syn_effects_test, x_test = load_best_avg_params(file_names, N_RULES, N_TIMECONSTS, 10)
 
-	print(x_test)
+	x_test[:N_RULES] = 0
 
 	eval_tracker = {
 		'evals': 0,
