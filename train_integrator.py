1527d60dedf9e69a15bf93cfa1cde10fd1e73c19
David Bell <davidgrebell@gmail.com>
2025-05-08 15:30:22 -0700

    grab first syn

--- train_integrator.py
+++ train_integrator.py
@@ -594,7 +594,7 @@ def simulate_all(keys, X, train, track_params=True):
 
 		print('w abs summed', np.abs(ws).sum())
 		print(syn.shape)
-		print('syn', jnp.mean(syn, axis=0))
+		print('syn', jnp.mean(syn[0, ...], axis=0))
 
 		if train_trial_flag or test_trial_flag:
 			if train_trial_flag:
