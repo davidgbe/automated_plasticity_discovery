6a929e725ea09b86a5303b9c6a307a44ac350320
David Bell <davidgrebell@gmail.com>
2025-05-08 10:28:09 -0700

    print a few more things

--- train_integrator.py
+++ train_integrator.py
@@ -215,9 +215,12 @@ def calc_loss(r_train, r_test, targets_train, targets_test):
 
 	w_screened = jnp.where(singular_matrices_detected, 0, w)
 
+	print(r_test_normed @ w_screened)
+
 	residual = jnp.square((targets_test_normed - r_test_normed @ w_screened)).sum()
 	total = jnp.square((targets_test_normed - targets_test_normed.mean())).sum()
 
+	print(targets_test_normed.shape)
 	print(residual)
 	print(total)
 	return jnp.where(invalid, 10, residual / total)
