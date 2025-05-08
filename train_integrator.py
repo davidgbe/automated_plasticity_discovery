96188621608166b17c8cec154b26bae44f8bfa98
David Bell <davidgrebell@gmail.com>
2025-05-08 10:58:23 -0700

    squeeze dot product

--- train_integrator.py
+++ train_integrator.py
@@ -212,16 +212,13 @@ def calc_loss(r_train, r_test, targets_train, targets_test):
 	RtR = jnp.matmul(jnp.transpose(r_train_normed), r_train_normed)
 	Rty = jnp.matmul(jnp.transpose(r_train_normed), targets_train_normed[:, None])
 
-	w = jnp.linalg.solve(RtR, Rty)
+	w = jnp.linalg.solve(RtR, Rty).squeeze(0)
 
 	singular_matrices_detected = jnp.any(jnp.isnan(w)) | jnp.any(jnp.isinf(w))
 
 	w_screened = jnp.where(singular_matrices_detected, 0, w)
 
-	print((r_test_normed @ w_screened).shape)
-	print(targets_test_normed.shape)
-
-	residual = jnp.square((targets_test_normed - r_test_normed @ w_screened)).sum()
+	residual = jnp.square((targets_test_normed - (r_test_normed @ w_screened).squeeze())).sum()
 	total = jnp.square(targets_test_normed).sum()
 
 	print(targets_test_normed)
