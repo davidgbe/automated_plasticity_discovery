d0bc5e530ae01d21c818c594624ebe0a97465d19
David Bell <davidgrebell@gmail.com>
2025-05-09 14:45:24 -0700

    print event mask

--- train_integrator.py
+++ train_integrator.py
@@ -466,6 +466,7 @@ def simulate_all(keys, X, train, track_params=True):
 		sol = simulate(t, ws, ws_plastic, r_in, c, tau_rules, n_e + n_i, DT, readout_times_for_trial, args, save_for_viewing=save_for_viewing)
 
 		print(sol)
+		print(sol.event_mask)
 
 		s, r_exp, W, syn = sol.ys
 		
