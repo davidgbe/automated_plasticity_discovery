2ce7536d10bd33e87595905f2021f6d1c59f0d66
David Bell <davidgrebell@gmail.com>
2025-05-09 14:29:12 -0700

    add scalingfor weight matrices

--- train_integrator.py
+++ train_integrator.py
@@ -470,6 +470,7 @@ def simulate_all(keys, X, train, track_params=True):
 		ws = W[-1, :]
 
 		if i % 5 == 0 and i > 0:
+			m = np.abs(ws[0, ...]).max()
 			plot_heatmap(ws[0, ...], cmap='bwr', vmin=-m, vmax=m, save_path=f'./figures/weight_matrix_{zero_pad(i, 3)}.png', figsize=(4, 3))
 			
 			r = jnp.transpose(jax_calc_r(s, s_offsets, g, n_e), (1, 0, 2))
