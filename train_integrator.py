c0cdd3de419f175422ebe0c9d7fa877739f80f82
David Bell <davidgrebell@gmail.com>
2025-05-15 10:52:22 -0700

    shorten sim too

--- train_integrator.py
+++ train_integrator.py
@@ -40,9 +40,9 @@ print(args)
 
 SEED = args.seed
 BATCH_SIZE = args.batch
-N_INNER_LOOP = 320 # Number of times to simulate network and plasticity rules per loss function evaluation
-decoder_train_trial_nums = (280, 300)
-decoder_test_trial_nums = (300, 320)
+N_INNER_LOOP = 10 # Number of times to simulate network and plasticity rules per loss function evaluation
+decoder_train_trial_nums = (6, 8)
+decoder_test_trial_nums = (8, 10)
 READOUTS_PER_TRIAL = 20
 STD_EXPL = args.std_expl
 DW_LAG = 5
