18737d3583204993998d81635d524aa58a532592
David Bell <davidgrebell@gmail.com>
2025-06-04 20:19:28 -0700

    try printing perturbation

--- test_sequence_massive_stoch_input.py
+++ test_sequence_massive_stoch_input.py
@@ -437,7 +437,8 @@ def simulate_single_network(index, x, train, track_params=True):
 		
 		disrupt_inputs_poisson = np.zeros((len(t), n_e + n_i))
 		disrupt_inputs_poisson[t_disrupt, :n_e][disrupt_targets] = 1
-		# disrupt_inputs_poisson[t_disrupt : t_disrupt + t_disrupt_len, :n_e][:, disrupt_targets] = np.random.poisson(lam=DISRUPT_RATE * dt, size=(t_disrupt_len, n_targeted))
+
+		print(disrupt_inputs_poisson[t_disrupt, :n_e])
 
 		r_in = poisson_arrivals_to_inputs(fixed_inputs_spks + random_inputs_poisson, 3e-3)
 		if i >= 400 and np.random.rand() < args.pert_prob:
