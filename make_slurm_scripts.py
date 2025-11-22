6042297fbee77e3bcfaf5bc7420ffdd6e975bcee
David Bell <davidgrebell@gmail.com>
2025-11-21 17:18:01 -0800

    label job names automatically

--- make_slurm_scripts.py
+++ make_slurm_scripts.py
@@ -3,6 +3,12 @@ from jinja2 import Template
 import json
 import os
 
+def zero_pad(s, n):
+    s_str = str(s)
+    pad = n - len(s_str)
+    zero_padding = '0' * pad
+    return zero_padding + s_str
+
 def main():
     parser = argparse.ArgumentParser(description="Generate job scripts from a template.")
     parser.add_argument("--template", required=True, help="Path to the template .sh file")
@@ -22,9 +28,11 @@ def main():
     # Ensure output directory exists
     os.makedirs(args.outdir, exist_ok=True)
 
+    
+
     # Generate output scripts
-    for params in param_list:
-        output_filename = os.path.join(args.outdir, f"job_{params['job_name']}.sh")
+    for i, params in enumerate(param_list):
+        output_filename = os.path.join(args.outdir, args.template[:-6] + zero_pad(i, 3) + '.slurm')
         with open(output_filename, "w") as out:
             out.write(template.render(**params))
         print("Wrote", output_filename)
