f5892b0cb7d54d8f904526d50e96923c1f850dce
David Bell <davidgrebell@gmail.com>
2025-11-26 10:13:42 -0800

    just revert input pattern to old one for testing

--- train_integrator.py
+++ train_integrator.py
@@ -548,22 +548,17 @@ def simulate_single_network(index, x, train, save_paths=None):
 	w_hist = []
 	all_weight_deltas = []
 	w_hist.append(w)
-
-	if args.train:
-		p_active = 0.1 + 0.9 * index / BATCH_SIZE
-	else:
-		p_active = 0.1 + 0.9 * index / TEST_REPEATS
 	
 	start = time.time()
 
 	all_syn_factors = []
 
+	if args.train:
+		input_len = int((index + 1) / (BATCH_SIZE + 1) * max_input_len)
+	else:
+		input_len = int((index + 1) / (TEST_REPEATS + 1) * max_input_len)
+
 	for i in range(n_inner_loop_iters):
-		# if i % 5 == 0:
-		# 	if i > 0:
-		# 		print('5 cycles:', time.time() - iter_start)
-		# 		sys.stdout.flush()
-			# iter_start = time.time()
 		# print(f'Activation number: {i}')
 		# Define input for activation of the network
 
@@ -573,9 +568,13 @@ def simulate_single_network(index, x, train, save_paths=None):
 		else:
 			input_spks = np.zeros((decoding_len_self_org + decoder_lag, 2 * n_e_side))
 			t_for_iter = t
-
-		n_input_blocks = max_input_len // input_block_timesteps
-		inputs = np.random.choice([0, -1, 1], size=n_input_blocks, p=[1-p_active, p_active/2, p_active/2])
+		
+		n_input_blocks = input_len // input_block_timesteps
+		inputs = np.zeros((n_input_blocks), dtype=int)
+		n_inputs = np.random.randint(0, n_input_blocks)
+		if n_inputs > 0:
+			inputs[:n_inputs] = np.random.choice([-1, 1], size=n_inputs)
+		np.random.shuffle(inputs)
 
 		for i_input_block, input_flag in enumerate(inputs):
 			k = i_input_block * input_block_timesteps
@@ -605,7 +604,7 @@ def simulate_single_network(index, x, train, save_paths=None):
 		r_in_spks[input_start:input_spks.shape[0] + decoder_lag + input_start, n_e_pool:n_e_pool + 2 * n_e_side] = input_spks
 		r_in = poisson_arrivals_to_inputs(r_in_spks, 3e-3)
 		
-		input_signal_totals.append(running_input_sums / max_input_len)
+		input_signal_totals.append(running_input_sums / input_len)
 
 		r_in[:, :n_e_pool]  = 0.25 * r_in[:, :n_e_pool]
 		r_in[:, n_e_pool:(n_e_pool + 2 * n_e_side)] = INPUT_AMP * r_in[:, n_e_pool:(n_e_pool + 2 * n_e_side)]
